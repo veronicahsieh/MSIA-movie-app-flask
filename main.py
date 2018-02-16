8b75de48f8936e4caf18f6408710430150ff667d
vhsieh920 <31772995+vhsieh920@users.noreply.github.com>
2018-02-16 16:39:42 -0600

    Updated file directory
    added test html files

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/') #root directory, homepage of the website
 def index():
-    return 'this is the homepage'
+    return render_template('index.html')
 
 if __name__ == "__main__": #quick check that only run app whenever this app is called directly
     app.run(debug=True) #start this app
 
 
-@app.route('/cluster/<genre>')
-def cluster(genre):
-
-@app.route('/about', method = ['GET','POST']) # this page can handle get and post requests
+@app.route('/about')
 def about():
-    if request.method == 'POST':
-        return 'you are using POST'
-    else:
-        return 'you are probably using GET'
+    return render_template('about.html')
