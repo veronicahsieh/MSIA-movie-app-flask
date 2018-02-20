bcd18e2d4b1d51c6b3f7f3fe248063f410811a82
vhsieh920 <31772995+vhsieh920@users.noreply.github.com>
2018-02-19 18:47:50 -0600

    Updated form code

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from flask import Flask, request, render_template
+from wtforms import Form, BooleanField, StringField, IntegerField, validators
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-@app.route('/') #root directory, homepage of the website
+csrf= CSRFProtect(app)
+
+class movie_form(FlaskForm):
+    #genre = SelectField(u'Movie Genres', choices = ['action','comedy','horror'])
+    release_year = IntegerField('Year')
+    rating = IntegerField('Movie Rating')
+    popularity = IntegerField('Popularity Scale', [validators.NumberRange(min = 1,max = 10)])
+    runtime = IntegerField('Runtime')
+    budget = IntegerField('Budget')
+
+@app.route('/',methods = ['GET','POST']) #root directory, homepage of the website
 def index():
-    return render_template('index.html')
+    form = movie_form()
+    return render_template('index.html', form = form)
 
 if __name__ == "__main__": #quick check that only run app whenever this app is called directly
     app.run(debug=True) #start this app
@@ -13,3 +27,8 @@ if __name__ == "__main__": #quick check that only run app whenever this app is c
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/inputdata', methods = ['POST'])
+def show_data():
+    if request.method == 'POST':
+        return request.form['genre']
