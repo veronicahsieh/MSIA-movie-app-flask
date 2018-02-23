c09d2447dbec5670a1a4bcdd3b70cbe2f2f9b437
vhsieh920 <31772995+vhsieh920@users.noreply.github.com>
2018-02-23 09:16:38 -0600

    updated scripts and reorganized sphinx docs

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import Flask, request, render_template
+from test_function import testfunction
+from genres_map import make_genres_map
+from predict import build_pred_frame, movie_pred
+import csv
+import datetime
+import pandas as pd
+import pickle
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 
@@ -10,7 +18,7 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/inputdata', methods = ['POST'])
+@app.route('/results', methods = ['POST'])
 def inputdata():
     if request.method == 'POST':
         genre = request.form['genre']
@@ -19,7 +27,16 @@ def inputdata():
         popularity = request.form['popularity']
         rating = request.form['rating']
         budget = request.form['budget']
-        return display
+
+        genres_map = {}
+        make_genres_map(genres_map,"genres_map.csv")
+
+        genres_code = genres_map.get(genre)
+
+        sample_frame = build_pred_frame(genres_code,year,runtime,popularity,rating,budget)
+        revenue = movie_pred(sample_frame)
+
+        return render_template("results.html",result = revenue)
 
 if __name__ == "__main__": #quick check that only run app whenever this app is called directly
     app.run(debug=True) #start this app
--- test_function.py
+++ test_function.py
@@ -1,1 +1,3 @@
-def testfunction():
+def testfunction(budget):
+    new_budget = int(budget) + 2
+    return new_budget
